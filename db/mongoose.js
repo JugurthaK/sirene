75957cd5340d9dc5bdbb1a20d1da3cb5c572c931
jugurthak <jugurthak@protonmail.com>
2021-11-28 18:55:17 +0100

    mongo never closing

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const { Company } = require('./CompanyModel');
 const insertInDatabase = (rows) => {
   return new Promise((resolve, reject) => {
-    mongoose.connect(`mongodb://localhost:27017/sirene`);
+    mongoose.connect(
+      `mongodb://localhost:27017/sirene?keepAlive=true&poolSize=30&autoReconnect=true&socketTimeoutMS=720000&connectTimeoutMS=720000`
+    );
     Company.insertMany(rows)
       .then((res) => resolve(res))
       .catch((err) => reject(err));
